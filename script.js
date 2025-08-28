7f16405ea6c80692b16dd2d52c15876f367a9e95
Federico Vincelli <vincellifederico@gmail.com>
2025-08-28 16:35:45 +0200

    Add buttons to UI

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const multiply = function(a,b) {
 const divide = function(a, b) {
     return a / b;
 };
+
+
+let num1; 
+let num2;
+let operator;
+
+const operate = function(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
