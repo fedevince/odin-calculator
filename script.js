748286f5dfd651ef74f7dfd1f27db5d65c33e887
Federico Vincelli <vincellifederico@gmail.com>
2025-08-30 23:30:58 +0200

    Add first version of executeOperation function

--- script.js
+++ script.js
@@ -31,3 +31,50 @@ const operate = function(num1, num2, operator) {
             return divide(num1, num2);
     }
 }
+
+const display = document.querySelector(".display");
+display.textContent = '';
+const operators = "-+*/";
+
+// fix this
+// set to save the last operator 
+// logic for equal
+// factor out splitting function?
+const executeOperation = function (display) { 
+    let operatorCounter = 0;
+    for (index in display.textContent) {
+        let character = display.textContent.charAt(index);
+        if (operators.includes(character)) operatorCounter ++;
+    }
+    if (operatorCounter >= 2) {
+        num1 = Number(display.textContent.split(/[+\-*\/]/)[0]);
+        num2 = Number(display.textContent.split(/[+\-*\/]/)[1]);
+        operator = display.textContent[display.textContent.search(/[+\-*\/]/)];
+        console.log(`num1 ${num1}, num2 ${num2}, operator ${operator}`)
+        let result = operate(num1, num2, operator);
+        display.textContent = result;
+    }
+}
+
+let btns = document.querySelectorAll("button");
+for (let btn of btns) {
+    btn.addEventListener("click", () => {
+        if (btn.id === "clear") display.textContent = 0;
+        else if (btn.id == "=") display.textContent = operate();
+        else display.textContent += btn.id;
+        executeOperation(display)
+    })
+}
+
+
+// function to execute operation on display
+// parse display content into num1, num2 and operator
+// set up a temporary result
+// set error for divide by 0
+// round results with long decimals
+// clear should clear all existing data
+// don't run any operation if operator is pressed two times
+// pressing a new digit when final result is displayed should clear everything
+
+// backspace button
+// keyboard support if focused
